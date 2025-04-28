6d8cc3a4dce47aea344f913fb3053e0f994c1b38
Dilshan Rodrigo <dilshanrodrigo@Dilshans-MacBook-Pro.local>
2025-04-28 12:03:53 +1000

    eight commit

--- vpc.tf
+++ vpc.tf
@@ -46,7 +46,7 @@ resource "aws_route_table" "public" {
 }
 
 #associate public routes
-resource "aws_route_table_association1" "public" {
+resource "aws_route_table_association" "public" {
     count = length(aws_subnet.public )
     subnet_id = aws_subnet.public [count.index].id
     route_table_id = aws_route_table.public.id
