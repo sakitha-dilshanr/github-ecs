1511e76f97484be0c09757ae0bc0219649b47a9a
Dilshan Rodrigo <dilshanrodrigo@Dilshans-MacBook-Pro.local>
2025-04-30 10:11:41 +1000

    43 commit

--- ecs-nginx-service.tf
+++ ecs-nginx-service.tf
@@ -47,7 +47,7 @@ resource "aws_ecs_service" "service-app" {
     launch_type = "FARGATE"
 
     network_configuration {
-      subnets = var.private_subnet_cidr
+      subnets = var.private_subnet_cidr.ids
       security_groups = [aws_security_group.ecs_service_sg.id]
 
     }
